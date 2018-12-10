489bf01eaacd342509166f1ffa3a56ce6556b6be
Yuan <992483331@qq.com>
2018-12-10 12:40:22 +0800

    build到github上

---
+++ build/precache-manifest.181c153ceb73396c2fa5451ba55d2d80.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "bac6c3ce93941303faa3",
+    "url": "/static/css/main.c536945e.chunk.css"
+  },
+  {
+    "revision": "679f235b4f10be7e59e105554a9f3cfe",
+    "url": "/static/media/baby5.679f235b.jpg"
+  },
+  {
+    "revision": "02d87f8575177a50baac",
+    "url": "/static/js/1.02d87f85.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bac6c3ce93941303faa3",
+    "url": "/static/js/main.bac6c3ce.chunk.js"
+  },
+  {
+    "revision": "7f6685e4cdf7226967d5432e2d604003",
+    "url": "/static/media/banner1.7f6685e4.jpg"
+  },
+  {
+    "revision": "3b60b265daeade79a04447edf14d0287",
+    "url": "/static/media/banner2.3b60b265.jpg"
+  },
+  {
+    "revision": "fa5c2156adff5ff86e71137920f044ca",
+    "url": "/static/media/banner3.fa5c2156.jpg"
+  },
+  {
+    "revision": "e3900ee707d7187986cc61bf72113174",
+    "url": "/static/media/banner4.e3900ee7.jpg"
+  },
+  {
+    "revision": "c3cf4497937b28ee8c6c61785ac4fa4a",
+    "url": "/static/media/banner5.c3cf4497.jpg"
+  },
+  {
+    "revision": "663eb9b89e2d4f2b5c3e0f51d9370501",
+    "url": "/static/media/baby1.663eb9b8.jpg"
+  },
+  {
+    "revision": "c5e8a4ea10cb8bc9779cdb9a6a4b9710",
+    "url": "/static/media/baby3.c5e8a4ea.jpg"
+  },
+  {
+    "revision": "02d87f8575177a50baac",
+    "url": "/static/css/1.2168e445.chunk.css"
+  },
+  {
+    "revision": "34c4f2e67653b844ae884fc7db2fae0e",
+    "url": "/index.html"
+  }
+];
